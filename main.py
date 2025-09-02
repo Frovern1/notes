0863fd87db9c75148cd5718752d6ab59d4c4ac82
Frovern1 <avdonicheva.polina@gmail.com>
2025-09-02 18:13:33 +0300

    main

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-import flask
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return render_template("index.html")
+
+@app.route("/about")
+def about_me():
+    return "about me:"
